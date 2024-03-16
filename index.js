a51b1efffe862b4475978f04598c44d082b6c4ef
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-03-16 12:44:32 +0000

    find product by it id using async

--- index.js
+++ index.js
@@ -5,21 +5,45 @@ require("dotenv").config();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log("[HOME]");
-    res.json({
-        "name": "Rana"
-    })
-})
+app.get("/", (req, res) => {
+  console.log("[HOME]");
+  res.json({
+    name: "Rana",
+  });
+});
 
+//get data from the database
+
+app.get("/api/products", (req, res) => {
+  try {
+    Product.find({}).then((data) => {
+      res.status(200).json(data);
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//get product by its id
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//add product data to the database
 app.post("/api/products", (req, res) => {
-    try {
-        Product.create(req.body).then((data) => {
-            res.status(200).json(data)
-        })
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+  try {
+    Product.create(req.body).then((data) => {
+      res.status(200).json(data);
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-module.exports = { app }
+module.exports = { app };
