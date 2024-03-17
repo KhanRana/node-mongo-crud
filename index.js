2ef9f61b33b6aa5e91bf8b057930b8c5c557e090
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-03-17 11:14:33 +0000

    add logic so if product not exist send the client message

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ app.get("/api/product/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const product = await Product.findById(id);
+    if (!product) {
+        return res.status(404).json({message: "Product not found!"});
+    }
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
